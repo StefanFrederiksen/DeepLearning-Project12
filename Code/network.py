8f509e81396f352f2ebe70f2c8309b0b1f4128d1
Stefan Frederiksen <s144469@student.dtu.dk>
2017-10-30 16:19:42 +0100

    Merge

---
+++ Code/network.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 13:11:18 2017
+
+@author: jacob
+"""
+
+import numpy as np
+import tensorflow as tf
+
+import utils 
+import keras
+from keras.layers import Dense, Conv2D, MaxPooling2D
+from tensorflow.contrib.layers import flatten
+
+path = ''
+show_dimensions = True
+
+load_model = True
+save_model = True
+batch_size = 32
+max_epochs = 10
+valid_every = 100
+
+"""
+todo: Data loader
+"""
+
+tf.reset_default_graph()
+
+num_classes = 10
+height, width, nchannels = 128, 128, 1
+padding = 'same'
+
+"""
+Parameters for network
+"""
+
+filters_1 = 1
+kernel_size_1 = (1,1) 
+pool_size_1 = (1,1)
+
+filters_2 = 1
+kernel_size_2 = (1,1)
+pool_size_2 = (1,1)
+
+x_pl = tf.placeholder(tf.float32, [None, height, width, nchannels], name='xPlaceholder')
+y_pl = tf.placeholder(tf.float32, [None, num_classes], name='yPlaceholder')
+
+"""
+The network
+"""
+with tf.variable_scope('convlayer1'):
+    conv1 = Conv2D(filters_1, kernel_size_1, strides=(2,2), padding=padding, activation='relu')
+    x = conv1(x_pl)   
+    pool1 = MaxPooling2D(pool_size=pool_size_1, strides=None, padding=padding)    
+    x = pool1(x)    
+    
+    if show_dimensions == True:
+        print('input\t\t', x_pl.get_shape())
+        print('output layer1\t', x.get_shape())
+        
+with tf.variable_scope('convlayer2'):
+    conv2 = Conv2D(filters_2, kernel_size_2, strides=(2,2), padding=padding, activation='relu')
+    x = conv2(x)
+    pool2 = MaxPooling2D(pool_size=pool_size_2, strides=None, padding=padding)
+    x = pool2(x)
+    
+with tf.variable_scope('output_layer'):
+    denseOut = Dense(units=num_classes, activation='softmax')
+    y = denseOut(x)
+    
+    if show_dimensions == True:
+        print('final output\t', y.get_shape())
+        
+"""
+Loss, Optimizer, Accuracy, etc
+"""
+with tf.variable_scope('loss'):
+    #cross entropy per sample
+    cross_entropy = -tf.reduce_sum(y_pl * tf.log(y+1e-8), axis=[1])
+    #average cross entropy
+    cross_entropy = tf.reduce_mean(cross_entropy)
+
+with tf.variable_scope('training'):
+    #define optimizer
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+    #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+
+    #apply it
+    train_op = optimizer.minimize(cross_entropy)
+    
+with tf.variable_scope('performance'):
+    #comparing results with labels
+    correct_prediction = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_pl, axis=1))
+    
+    """
+    todo: Implement correct_prediction when multi labels present
+    """
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+"""
+Training loop
+"""
+
+
+valid_loss, valid_accuracy = [],[]
+train_loss, train_accuracy = [],[]
+test_loss, test_accuracy = [],[]
+saver = tf.train.Saver()
+batches_completed = 0 
+epochs_completed = 0
+
+with tf.Session() as sess:
+    if load_model == True:
+        saver.restore(sess, "../model/model.ckpt")
+        print("Model restored")
+    else:
+        sess.run(tf.global_variables_initializer())
+    print("\ttrain_loss \ttrain_acc \tvalid_loss \tvalid_acc")
+    try:
+        while epochs_completed < max_epochs:
+            _train_loss, _train_accuracy = [],[]
+            
+            # Run training
+            x_batch, y_batch = "data loader here"
+            fetches_train = [train_op, cross_entropy, accuracy]
+            feed_dict_train = {x_pl: x_batch, y_pl: y_batch}
+            _, _loss, _acc = sess.run(fetches_train, feed_dict_train)
+            
+            _train_loss.append(_loss)
+            _train_accuracy.append(_acc)
+            
+            # Compute validation, loss and accuracy
+            if batches_completed % valid_every == 0:
+                train_loss.append(np.mean(_train_loss))
+                train_accuracy.append(np.mean(_train_accuracy))
+                
+                fetches_valid = [cross_entropy, accuracy]
+                
+                """
+                todo: valid samples hentes
+                """
+                feed_dict_valid = {x_pl: x_valid, y_pl: y_valid}
+                _loss, _acc = sess.run(fetches_valid, feed_dict_valid)
+                
+                valid_loss.append(_loss)
+                valid_accuracy.append(_acc)
+                
+                print("%d:\t  %.2f\t\t  %.1f\t\t  %.2f\t\t  %.1f" \
+                      % (batches_completed, train_loss[-1], train_accuracy[-1], \
+                         valid_loss[-1], valid_accuracy[-1]))
+                
+                
+    except KeyboardInterrupt:
+        pass
+    
+    
+    
+    
+    if save_model == True:
+        save_path = (sess, "../model/model.ckpt")
+        print("Model saved in file: %s" % save_path)
---
+++ Code/spec_generator.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 13:43:34 2017
+
+@author: jacob
+"""
+ 
+import numpy as np
+import scipy.signal
+import os
+import librosa
+from sklearn.preprocessing import normalize
+
+
+path_to_8k = '/home/jacob/Downloads/UrbanSound8K'
+save_path = path_to_8k+'/spectograms'
+path = path_to_8k+'/audio'
+
+path = '/media/jacob/Files/UrbanSound8K/audio/'
+os.mkdir(save_path)    
+
+
+i = 0
+folders = os.listdir(path)
+for folder in folders:
+    if folder[0:5] == 'fold4':
+        os.mkdir(save_path+'/'+folder)
+        path_temp = path+'/'+folder
+        files = os.listdir(path_temp)
+        for file in files:
+            if file[-4:] == '.wav':
+                try:
+                    sound, sample_rate = librosa.load(path_temp+'/'+file)
+                    f, t, sxx = scipy.signal.spectrogram(sound, sample_rate, noverlap=64)
+                    sxx = normalize(sxx[1:])
+                    for i in range(int((np.shape(sxx)[1]-np.shape(sxx)[1]%128)/128)):
+                        temp = sxx[:,(i)*128:(i+1)*128]
+                        np.savetxt(save_path+'/'+folder+'/'+file[:-4]+'_'+str(i)+'.txt', temp, delimiter=',')
+                except:
+                    i += 1
+  
+                    
